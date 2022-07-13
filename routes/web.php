ce9fda84e47c3bf53c00a3b2fb02f515202ee09a
Takshak Tiwari <takshaktiwari@gmail.com>
2022-07-13 13:24:41 +0530

    conditional routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Takshak\Agallery\Http\Controllers\GalleryController;
 
-Route::middleware(['web'])->prefix('galleries')->name('galleries.')->group(function () {
-    Route::get('/', [GalleryController::class, 'index'])->name('index');
-    Route::get('{gallery:slug}', [GalleryController::class, 'show'])->name('show');
-});
+if (config('site.gallery.routes', true)) {
+    Route::middleware(['web'])->prefix('galleries')->name('galleries.')->group(function () {
+        Route::get('/', [GalleryController::class, 'index'])->name('index');
+        Route::get('{gallery:slug}', [GalleryController::class, 'show'])->name('show');
+    });
+}
